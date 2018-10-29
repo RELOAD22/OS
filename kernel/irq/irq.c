35ebabc17e742610692449506bc32db7394128cd
RELOAD22 <dizhanyuan16@mails.ucas.ac.cn>
2018-10-29 16:12:52 +0800

    task 3

--- kernel/irq/irq.c
+++ kernel/irq/irq.c
@@ -7,7 +7,7 @@ static void irq_timer()
 {
     // TODO clock interrupt handler.
     // scheduler, time counter in here to do, emmmmmm maybe.
-    
+    scheduler();
 }
 
 void interrupt_helper(uint32_t status, uint32_t cause)
